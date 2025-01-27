6d9c3e32e977fcd70aa07385de66a4e3c7a3e6e3
= <=>
2025-01-28 05:08:49 +0900

    Add initial project files and configurations for Flutter application

---
+++ android/app/src/main/kotlin/com/example/receive_sharing_files/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.receive_sharing_files
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:receive_sharing_intent/receive_sharing_intent.dart';
+import 'package:logger/logger.dart';
+
+final logger = Logger();
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  late StreamSubscription _intentSub;
+  final _sharedFiles = <SharedMediaFile>[];
+
+  @override
+  void initState() {
+    super.initState();
+
+    logger.d("initState");
+
+    // Listen to media sharing coming from outside the app while the app is in the memory.
+    _intentSub = ReceiveSharingIntent.instance.getMediaStream().listen((value) {
+      setState(() {
+        _sharedFiles.clear();
+        _sharedFiles.addAll(value);
+
+        logger.d(_sharedFiles.map((f) => f.toMap()));
+      });
+    }, onError: (err) {
+      logger.e("getIntentDataStream error: $err");
+    });
+
+    // Get the media sharing coming from outside the app while the app is closed.
+    ReceiveSharingIntent.instance.getInitialMedia().then((value) {
+      setState(() {
+        _sharedFiles.clear();
+        _sharedFiles.addAll(value);
+        logger.d(_sharedFiles.map((f) => f.toMap()));
+
+        // Tell the library that we are done processing the intent.
+        ReceiveSharingIntent.instance.reset();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _intentSub.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const textStyleBold = TextStyle(fontWeight: FontWeight.bold);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Text("Shared files:", style: textStyleBold),
+              Text(_sharedFiles
+                  .map((f) => f.toMap())
+                  .join(",\n****************\n")),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
